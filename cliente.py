53911bbf66417a75228e04bbd23ac85a49895051
Felipe Ferreira <felipeffbs3x@gmail.com>
2018-11-16 19:40:13 -0300

    added function wich select players turn

--- cliente.py
+++ cliente.py
@@ -1,7 +1,17 @@
 import xmlrpc.client
-
+import time
 s = xmlrpc.client.ServerProxy('http://localhost:9999')
 
 
 player_name = str(input())
 player = s.login(player_name)
+
+while(True):
+    if(s.my_turn(player)):
+        print("My turn")
+        time.sleep(1)
+        #it's my turn
+    else:
+        print("NOT My turn")
+        time.sleep(5)
+        #it's not my turn
--- server.py
+++ server.py
@@ -4,34 +4,33 @@ import time
 
 server = SimpleXMLRPCServer(('localhost', 9999))
 
-def initialize_board() -> list:
-    board = np.zeros((7,7), dtype=int)
-    for i in range(0,7):
-            for j in range(0,7):
-                if(i==0 or i ==6):
-                    if (j == 0 or j == 3 or j==6):
-                        board[i,j] = 5
-                if(i==1 or i ==5):
-                    if (j == 1 or j == 3 or j==5):
-                        board[i,j] = 5
-                if(i==2 or i ==4):
-                    if (j == 2 or j == 3 or j==4):
-                        board[i,j] = 5
-                if(i == 3):
-                    if (j == 0 or j == 1 or j==2 or j == 4 or j == 5 or j==6):
-                        board[i,j] = 5
-    return board
-
-
 players = []
 def login(player_name: str) -> int:
     if(len(players) < 2):
         players.append(player_name)
         print(player_name+" connected to the server")
+        #if(len(players) == 1):
+        #    player_turn = 1
         return len(players)
     else:
         print(player_name+" tryed to connect...")
         return 0
-server.register_function(login, "login")
+player_turn = 1
+def turn(player: int) -> bool:
+    global player_turn
+    if(player_turn == player):
+        if(player_turn == 1):
+            player_turn = 2
+        else:
+            player_turn = 1
+        return True
+    else:
+        return False
 
+
+
+
+
+server.register_function(turn, "my_turn")
+server.register_function(login, "login")
 server.serve_forever()
