fbb7c7ec5dc8d43b6ee0e9789d34e373be374b09
Felipe Ferreira <felipeffbs3x@gmail.com>
2018-11-16 20:32:58 -0300

    Players wait for both players ready and he plays only he turns.

--- cliente.py
+++ cliente.py
@@ -2,9 +2,17 @@ import xmlrpc.client
 import time
 s = xmlrpc.client.ServerProxy('http://localhost:9999')
 
-
+print("Welcome to the Nine Men's Morris game\nWhat's your name player?")
 player_name = str(input())
 player = s.login(player_name)
+
+if(player == 0):
+    print("Sorry "+player_name+" but this game already have two players...")
+    exit()
+
+while(s.ready()):
+    print("Waiting for another player...")
+    time.sleep(2)
 
 while(True):
     if(s.my_turn(player)):
--- server.py
+++ server.py
@@ -1,21 +1,23 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import numpy as np 
 import time
+import random
 
 server = SimpleXMLRPCServer(('localhost', 9999))
-
+player_turn = 0
 players = []
 def login(player_name: str) -> int:
+    global player_turn
     if(len(players) < 2):
         players.append(player_name)
         print(player_name+" connected to the server")
-        #if(len(players) == 1):
-        #    player_turn = 1
+        player_turn = random.randint(1,2)
+        print(player_turn)
         return len(players)
     else:
         print(player_name+" tryed to connect...")
         return 0
-player_turn = 1
+
 def turn(player: int) -> bool:
     global player_turn
     if(player_turn == player):
@@ -23,14 +25,19 @@ def turn(player: int) -> bool:
             player_turn = 2
         else:
             player_turn = 1
+        print(players[player-1]+'\'s turn')
+        return True
+    else:
+        return False
+
+def players_ready() -> bool:
+    if(len(players) < 2):
         return True
     else:
         return False
 
 
-
-
-
+server.register_function(players_ready, "ready")
 server.register_function(turn, "my_turn")
 server.register_function(login, "login")
 server.serve_forever()
