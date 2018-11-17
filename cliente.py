a0125e020bc51d02ae07ba919ecb1e2bcf4b54a9
Felipe Ferreira <felipeffbs3x@gmail.com>
2018-11-17 14:31:10 -0300

    added functions wich send player's map and place a piece in the right place in the server

--- cliente.py
+++ cliente.py
@@ -2,6 +2,24 @@ import xmlrpc.client
 import time
 s = xmlrpc.client.ServerProxy('http://localhost:9999')
 
+def print_board():
+    board = s.board()
+    print("A B C D E F G")
+    print("-------------")
+    count = 0
+    while(count!=49):
+        for i in range(0,7):
+            if(board[count] == '5'):
+                print("O", end=' ')
+            elif(board[count] == '0'):
+                print(" ", end=' ')
+            elif(board[count] == '1'):
+                print("1", end=' ')
+            elif(board[count] == '2'):
+                print("2", end=' ')
+            count+=1
+        print("|"+ str(int(count/7)))
+
 print("Welcome to the Nine Men's Morris game\nWhat's your name player?")
 player_name = str(input())
 player = s.login(player_name)
@@ -10,15 +28,19 @@ if(player == 0):
     print("Sorry "+player_name+" but this game already have two players...")
     exit()
 
+print_board()
+
 while(s.ready()):
     print("Waiting for another player...")
     time.sleep(2)
 
+print("Time to place your pieces")
 while(True):
     if(s.my_turn(player)):
+        #it's my turn
         print("My turn")
         time.sleep(1)
-        #it's my turn
+        
     else:
         print("NOT My turn")
         time.sleep(5)
--- server.py
+++ server.py
@@ -17,6 +17,8 @@ def login(player_name: str) -> int:
     else:
         print(player_name+" tryed to connect...")
         return 0
+server.register_function(login, "login")
+
 
 def turn(player: int) -> bool:
     global player_turn
@@ -29,15 +31,72 @@ def turn(player: int) -> bool:
         return True
     else:
         return False
+server.register_function(turn, "my_turn")
+
 
 def players_ready() -> bool:
     if(len(players) < 2):
         return True
     else:
         return False
-
-
 server.register_function(players_ready, "ready")
-server.register_function(turn, "my_turn")
-server.register_function(login, "login")
+
+
+def init_board():
+    board = np.zeros((7,7), dtype = int)
+    for i in range(0,7):
+        for j in range(0,7):
+            if(i==0 or i ==6):
+                if (j == 0 or j == 3 or j==6):
+                    board[i,j] = 5
+            if(i==1 or i ==5):
+                if (j == 1 or j == 3 or j==5):
+                    board[i,j] = 5
+            if(i==2 or i ==4):
+                if (j == 2 or j == 3 or j==4):
+                    board[i,j] = 5
+            if(i == 3):
+                if (j == 0 or j == 1 or j==2 or j == 4 or j == 5 or j==6):
+                    board[i,j] = 5
+    return board
+board = init_board()
+
+def get_board():
+    str_board = []
+    for i in range(0,7):
+        for j in range(0,7):
+            str_board.append(str(board[i,j]))
+    return str_board
+server.register_function(get_board, "board")
+
+
+def piece_placer(line: int, colu: int, player: int):
+    board[line, column] = player
+
+def valid_place(line: int, colu: int) -> bool:
+    if(board[line, colu] == 0 or board[line, colu] == 1 or board[line, colu] == 2):
+        return False
+    else:
+        return True
+
+def line_colu(place: str):
+    print(place)
+    line = int(place[0]) - 1
+    column = int(ord(place[1]) - ord('A'))
+    return line, column
+
+def place_piece(place: str, player: int) -> bool:
+    l, c = line_colu(place)
+    if(valid_place(l, c)):
+        print("Player "+players[player]+" place a piece in "+place)
+        piece_placer(l, c, player)
+        return True
+    else:
+        print("Player "+players[player]+" tryed to place a piece in "+place)
+        return False
+server.register_function(place_piece, "place_piece")
+
+
+
+
 server.serve_forever()
