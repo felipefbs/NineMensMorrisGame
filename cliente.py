09807a6db9bdd1e96ff8e27dfff5bacb13a0508f
Felipe <34188166+felipefbs@users.noreply.github.com>
2018-12-08 15:38:37 -0300

    added socket com
    now server send some messages to the client by sockets. but need a lot more refact in this code

--- cliente.py
+++ cliente.py
@@ -1,49 +1,50 @@
+import socket
 import xmlrpc.client
 import time
-import sys
-#import os
-ip = sys.argv[1]
-if not ip:
-    print("Server IP Address:")
-    ip = str(input())
-s = xmlrpc.client.ServerProxy('http://'+ip+':10001')
+
+
+def client_ip():
+    ip_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_sock.connect(('8.8.8.8', 80))
+    return ip_sock.getsockname()[0]
+
+client_ip = client_ip()
+ip_server = '127.0.0.1'
+port = 10000
+server = xmlrpc.client.ServerProxy('http://'+ip_server+':'+str(port))
 
 def print_board():
-    board = s.board()
-    #os.system("clear")
-    print("A B C D E F G")
-    print("-------------")
-    count = 0
-    while(count!=49):
-        for i in range(0,7):
-            if(board[count] == '5'):
-                print("O", end=' ')
-            elif(board[count] == '0'):
-                print(" ", end=' ')
-            elif(board[count] == '1'):
-                print("1", end=' ')
-            elif(board[count] == '2'):
-                print("2", end=' ')
-            count+=1
-        print("|"+ str(int(count/7)))
+    board = server.board()
+
+    print('  A   B   C   D   E   F   G')
+    print('1 '+board[0]+'-----------'+board[1]+'-----------'+board[2])
+    print('  |           |           |')
+    print('2 |   '+board[3]+'-------'+board[4]+'-------'+board[5]+'   |')
+    print('  |   |       |       |   |')
+    print('3 |   |   '+board[6]+'---'+board[7]+'---'+board[8]+'   |   |')
+    print('  |   |   |       |   |   |')
+    print('4 '+board[9]+'---'+board[10]+'---'+board[11]+'       '+board[12]+'---'+board[13]+'---'+board[14])
+    print('  |   |   |       |   |   |')
+    print('5 |   |   '+board[15]+'---'+board[16]+'---'+board[17]+'   |   |')
+    print('  |   |       |       |   |')
+    print('6 |   '+board[18]+'-------'+board[19]+'-------'+board[20]+'   |')
+    print('  |           |           |')
+    print('7 '+board[21]+'-----------'+board[22]+'-----------'+board[23])
 
 def verify_mill(place: str, player: int, enemy_player: int):
-    if (s.verify(place, player)):
+    if (server.verify(place, player)):
         print_board()
         print("Select a piece from enemy player to remove")
         place = str(input()).upper()
-        if(not(s.remove(place, enemy_player))):
+        if(not(server.remove(place, enemy_player))):
             print("Select a piece from enemy player to remove")
             place = str(input()).upper()
 
-print("Welcome to the Nine Men's Morris game\nWhat's your name player?")
-player_name = str(input())
-player = s.login(player_name)
-
-if(player == 0):
-    print("Sorry "+player_name+" but this game already have two players...")
-    exit()
-
+name = str(input('What\'s your name, player?  '))
+player = server.login(name, client_ip)
+print(player)
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.bind((client_ip, (port+player)))
 
 if (player == 1):
     enemy_player = 2
@@ -51,68 +52,75 @@ else:
     enemy_player = 1
 
 print_board()
+client_sock.listen(10)
+server.im_ready(player)
+receiver, address = client_sock.accept()
 
-while(s.ready()):
+
+while (receiver.recv(8).decode('utf-8') == 'wait'):
     print("Waiting for another player...")
-    time.sleep(3)
-
-
+    time.sleep(1)
+else:
+    print("Player found. Let the game begin!")
+ 
+client_sock.close()
 pieces_in_board = 0
-max_pieces = 9
-print("Time to place your pieces")
-while(pieces_in_board < max_pieces):
-    while(s.my_turn(player)):
+max_pieces = 3
+print("Time to place your pieces into the board")
+server.game_stage(1)
+turn = 0
+while (pieces_in_board < max_pieces):
+    turn = receiver.recv(8).decode('utf-8')
+    if( turn == str(player)):
         print_board()
         print("Remaing pieces: " + str(max_pieces - pieces_in_board))
-        print("Where you want to place your piece?")
-        place = str(input()).upper().replace(" ", "")
-        while(not(s.place_piece(place, player))):
+        place = str(input("Where you want to place your piece?  ")).upper().replace(" ", "")
+        while(not(server.place(place, player))):
             print("Invalid place\nIndicate a valid place!")
             place = str(input()).upper().replace(" ", "")
         pieces_in_board += 1
         verify_mill(place, player, enemy_player)
-        s.not_my_turn()      
+        server.not_my_turn()
     else:
         print_board()
         print("Enemy turn...")
         print("Remaing pieces: " + str(max_pieces - pieces_in_board))
-        time.sleep(2)
-
 
 print_board()
-print("Time to move your pieces arround the board to decide whom wins!")
-game = 0   
-while(game == 0):
-    if(s.my_turn(player)):
+server.game_stage(2)
+
+game = 0
+while (game == 0):
+    turn = receiver.recv(8).decode('utf-8')
+    if( turn == str(player)):
         print_board()
         print("You have " + str(pieces_in_board)+" pieces in the board")
-        
         print("Wich piece you want to move?")
         curr_place = str(input()).upper().replace(" ", "")
         print("To where you wanna to move your piece?")
         next_place = str(input()).upper().replace(" ", "")
-
-        while (not s.move(curr_place, next_place, player)):
+        while (not server.move(curr_place, next_place, player)):
             print("You tried to select an invalid piece or an invalid place\nDo it right this time")
             curr_place = str(input()).upper().replace(" ", "")
             print("To where you wanna to move your piece?")
             next_place = str(input()).upper().replace(" ", "")
         print_board()
-        
-        verify_mill(next_place, player, enemy_player)
-
-        print_board()
-        s.not_my_turn()
+        verify_mill(place, player, enemy_player)
+        server.not_my_turn()        
     else:
         print_board()
         print("Enemy turn...")
         print("You have " + str(pieces_in_board)+" pieces in the board")
-        time.sleep(3)
+    server.game()
 
-    game = s.end_game()
+    
 
-if(game == 1):
+if(game == player):
     print("Congratulations, you win")
-elif(game == 2):
+else:
     print("Sorry buddy, but you loose the game")
+
+    
+    
+
 
--- server.py
+++ server.py
@@ -1,83 +1,81 @@
 from xmlrpc.server import SimpleXMLRPCServer
-import numpy as np
+import socket
+import threading
 import time
-import random
+import numpy as np
 
-
-ip = '127.0.0.1'
-server = SimpleXMLRPCServer((ip, 10001))
-player_turn = 0
+ip_server = '127.0.0.1'
+port = 10000
+server = SimpleXMLRPCServer((ip_server, port), allow_none=True)
 players = []
-def login(player_name: str) -> int:
-    global player_turn
-    if(len(players) < 2):
-        players.append(player_name)
-        print(player_name+" connected to the server")
-        player_turn = random.randint(1,2)
-        print(player_turn)
-        return len(players)
-    else:
-        print(player_name+" tryed to connect...")
-        return 0
-server.register_function(login, "login")
+sock_c1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+player_turn = 1
+game_stage = 0
+
+def login(player_name: str, client_ip: str):
+        print(player_name)
+        print(client_ip)
+        if(len(players) < 2):
+                players.append([player_name, client_ip])
+                print(players)
+                return len(players)
+        else:
+                return 0
+server.register_function(login, 'login')
 
 
-def my_turn(player: int) -> bool:
-    global player_turn
-    if(player_turn == player):
-        print(players[player-1]+'\'s turn')
-        return True
-    else:
-        return False
-server.register_function(my_turn, "my_turn")
+def im_ready(player: int):
+        global player1_ready 
+        global player2_ready
+        if (player == 1):
+                sock_c1.connect((players[0][1], 10001))
+                return 0
+        else:   
+                sock_c2.connect((players[1][1], 10002))
+                return 0
+server.register_function(im_ready, 'im_ready')
 
 
 def not_my_turn():
-    global player_turn
-    if(player_turn == 1):
-        player_turn = 2
-        return 0
-    else:
-        player_turn = 1
-        return 0
+        global player_turn
+        print(player_turn)
+        if(player_turn == 1):
+                player_turn = 2
+                #sock_c2.send("2".encode('utf-8'))
+        else:
+                player_turn = 1
+                #sock_c1.send("2".encode('utf-8'))
 server.register_function(not_my_turn, "not_my_turn")
 
-
-def players_ready() -> bool:
-    if(len(players) < 2):
-        return True
-    else:
-        return False
-server.register_function(players_ready, "ready")
-
-
 def init_board():
-    board = np.zeros((7,7), dtype = int)
-    for i in range(0,7):
-        for j in range(0,7):
-            if(i==0 or i ==6):
-                if (j == 0 or j == 3 or j==6):
-                    board[i,j] = 5
-            if(i==1 or i ==5):
-                if (j == 1 or j == 3 or j==5):
-                    board[i,j] = 5
-            if(i==2 or i ==4):
-                if (j == 2 or j == 3 or j==4):
-                    board[i,j] = 5
-            if(i == 3):
-                if (j == 0 or j == 1 or j==2 or j == 4 or j == 5 or j==6):
-                    board[i,j] = 5
-    return board
+        board = np.zeros((7,7), dtype = int)
+        board[0] = [5, 0, 0, 5, 0, 0, 5]
+        board[1] = [0, 5, 0, 5, 0, 5, 0]
+        board[2] = [0, 0, 5, 5, 5, 0, 0]
+        board[3] = [5, 5, 5, 0, 5, 5, 5]
+        board[4] = board[2]
+        board[5] = board[1]
+        board[6] = board[0]
+
+        print (board) 
+        return board
 board = init_board()
 
 def get_board():
-    str_board = []
-    for i in range(0,7):
-        for j in range(0,7):
-            str_board.append(str(board[i,j]))
-    return str_board
-server.register_function(get_board, "board")
+        str_board = ''
+        for i in range(0,7):
+                for j in range(0,7):
+                        str_board += str(board[i,j])
+        return str_board.replace('0', '').replace('5', 'O')
+server.register_function(get_board, 'board')
 
+def game_stage_changer(stage: int):
+        global game_stage
+        game_stage = stage
+        #sock_c1.send("1".encode('utf-8'))
+
+server.register_function(game_stage_changer, 'game_stage')
 
 def piece_placer(line: int, column: int, player: int):
     board[line, column] = player
@@ -105,15 +103,61 @@ def place_piece(place: str, player: int) -> bool:
         l, c = line_column(place)
         if(valid_place(l, c)):
             print(player)
-            print("Player "+players[player-1]+" place a piece in "+place)
+            print("Player "+players[player-1][0]+" place a piece in "+place)
             piece_placer(l, c, player)
             return True
         else:
-            print("Player "+players[player-1]+" tryed to place a piece in "+place)
+            print("Player "+players[player-1][0]+" tryed to place a piece in "+place)
             return False
     else:
         return False
-server.register_function(place_piece, "place_piece")
+server.register_function(place_piece, "place")
+
+
+def is_mill(player: int, place1: str, place2: str) -> bool:
+    if (board[line_column(place1)] == player and board[line_column(place2)] == player):
+        return True
+    return False
+
+def verify_mill(place: str, player: int) -> bool:
+    mill = {    '1A': (is_mill(player, '1D', '1G')) or (is_mill(player, '4A', '7A')),
+                '1D': (is_mill(player, '1G', '1A')) or (is_mill(player, '2D', '3D')),
+                '1G': (is_mill(player, '1D', '1A')) or (is_mill(player, '4G', '7G')),
+                '2B': (is_mill(player, '2D', '2F')) or (is_mill(player, '4B', '6A')),
+                '2D': (is_mill(player, '2B', '2F')) or (is_mill(player, '1D', '3D')),
+                '2F': (is_mill(player, '4F', '6F')) or (is_mill(player, '2D', '2B')),
+                '3C': (is_mill(player, '3D', '3E')) or (is_mill(player, '4C', '5C')),
+                '3D': (is_mill(player, '3C', '3E')) or (is_mill(player, '1D', '2D')),
+                '3E': (is_mill(player, '4E', '5E')) or (is_mill(player, '3C', '3D')),
+                '4A': (is_mill(player, '1A', '7A')) or (is_mill(player, '4B', '4C')),
+                '4B': (is_mill(player, '4A', '4C')) or (is_mill(player, '2B', '6B')),
+                '4C': (is_mill(player, '3C', '5C')) or (is_mill(player, '4A', '4B')),
+                '4E': (is_mill(player, '4F', '4G')) or (is_mill(player, '3E', '5E')),
+                '4F': (is_mill(player, '2F', '6F')) or (is_mill(player, '4E', '4G')),
+                '4G': (is_mill(player, '1G', '7G')) or (is_mill(player, '4E', '4F')),
+                '5C': (is_mill(player, '3C', '4C')) or (is_mill(player, '5D', '5E')),
+                '5D': (is_mill(player, '5C', '5E')) or (is_mill(player, '6D', '7D')),
+                '5E': (is_mill(player, '5C', '5D')) or (is_mill(player, '3E', '4E')),
+                '6B': (is_mill(player, '6D', '6F')) or (is_mill(player, '2B', '4B')),
+                '6D': (is_mill(player, '6B', '6F')) or (is_mill(player, '5D', '7D')),
+                '6F': (is_mill(player, '6D', '6B')) or (is_mill(player, '2F', '4F')),
+                '7A': (is_mill(player, '1A', '4A')) or (is_mill(player, '7D', '7G')),
+                '7D': (is_mill(player, '7A', '7G')) or (is_mill(player, '5D', '6D')),
+                '7G': (is_mill(player, '7A', '7D')) or (is_mill(player, '1G', '4G'))}
+    return mill[place]
+server.register_function(verify_mill, "verify")
+
+def remove_piece(place: str, enemy_player: int) -> bool:
+    l, c = line_column(place)
+    if (0 <= l <= 6 and 0 <= c <= 6):
+        if (board[l,c] == enemy_player):
+            piece_placer(l, c, 5)
+            return True
+        else:
+            return False
+    else:
+        return False
+server.register_function(remove_piece, "remove")
 
 def move_piece(curr_place: str, next_place: str, player: int) -> bool:
     # se o local atual for uma peça do jogador e o proximo local for um local vazio e ambos estiverem em locais dentro do tabuleiro a função executa o movimento
@@ -131,63 +175,7 @@ def move_piece(curr_place: str, next_place: str, player: int) -> bool:
             return False
     else:
         return False
-server.register_function(move_piece, "move")   
-
-
-def remove_piece(place: str, enemy_player: int) -> bool:
-    l, c = line_column(place)
-    if (0 <= l <= 6 and 0 <= c <= 6):
-        if (board[l,c] == enemy_player):
-            piece_placer(l, c, 5)
-            return True
-        else:
-            return False
-    else:
-        return False
-server.register_function(remove_piece, "remove")
-
-def is_mill(player: int, place1: str, place2: str) -> bool:
-    if (board[line_column(place1)] == player and board[line_column(place2)] == player):
-        return True
-    return False
-'''
-1A        1D       1G
-    2B    2D    2F
-       3C 3D 3E
-4A  4B 4C    4E 4F 4G
-       5C 5D 5E
-    6B    6D    6F
-7A        7D       7G
-'''
-def verify_mill(place: str, player: int) -> bool:
-    mill = { 
-        '1A': (is_mill(player, '1D', '1G')) or (is_mill(player, '4A', '7A')),
-        '1D': (is_mill(player, '1G', '1A')) or (is_mill(player, '2D', '3D')),
-        '1G': (is_mill(player, '1D', '1A')) or (is_mill(player, '4G', '7G')),
-        '2B': (is_mill(player, '2D', '2F')) or (is_mill(player, '4B', '6A')),
-        '2D': (is_mill(player, '2B', '2F')) or (is_mill(player, '1D', '3D')),
-        '2F': (is_mill(player, '4F', '6F')) or (is_mill(player, '2D', '2B')),
-        '3C': (is_mill(player, '3D', '3E')) or (is_mill(player, '4C', '5C')),
-        '3D': (is_mill(player, '3C', '3E')) or (is_mill(player, '1D', '2D')),
-        '3E': (is_mill(player, '4E', '5E')) or (is_mill(player, '3C', '3D')),
-        '4A': (is_mill(player, '1A', '7A')) or (is_mill(player, '4B', '4C')),
-        '4B': (is_mill(player, '4A', '4C')) or (is_mill(player, '2B', '6B')),
-        '4C': (is_mill(player, '3C', '5C')) or (is_mill(player, '4A', '4B')),
-        '4E': (is_mill(player, '4F', '4G')) or (is_mill(player, '3E', '5E')),
-        '4F': (is_mill(player, '2F', '6F')) or (is_mill(player, '4E', '4G')),
-        '4G': (is_mill(player, '1G', '7G')) or (is_mill(player, '4E', '4F')),
-        '5C': (is_mill(player, '3C', '4C')) or (is_mill(player, '5D', '5E')),
-        '5D': (is_mill(player, '5C', '5E')) or (is_mill(player, '6D', '7D')),
-        '5E': (is_mill(player, '5C', '5D')) or (is_mill(player, '3E', '4E')),
-        '6B': (is_mill(player, '6D', '6F')) or (is_mill(player, '2B', '4B')),
-        '6D': (is_mill(player, '6B', '6F')) or (is_mill(player, '5D', '7D')),
-        '6F': (is_mill(player, '6D', '6B')) or (is_mill(player, '2F', '4F')),
-        '7A': (is_mill(player, '1A', '4A')) or (is_mill(player, '7D', '7G')),
-        '7D': (is_mill(player, '7A', '7G')) or (is_mill(player, '5D', '6D')),
-        '7G': (is_mill(player, '7A', '7D')) or (is_mill(player, '1G', '4G'))
-        }
-    return mill[place]
-server.register_function(verify_mill, "verify")
+server.register_function(move_piece, "move")
 
 def end_game():
     player1_pieces = 0
@@ -204,9 +192,38 @@ def end_game():
             return 1
         else:
             return 2
-    
     return 0
-server.register_function(end_game, "end_game")
-
-
+server.register_function(end_game, 'game')
+def game():
+        global player_turn
+        while (game_stage == 1 or game_stage == 0):
+                if(game_stage == 0):
+                        time.sleep(1)
+                        if(len(players) == 1):
+                                sock_c1.send("wait".encode('utf-8'))
+                        elif(len(players) == 2):
+                                sock_c1.send("ready".encode('utf-8'))
+                                sock_c2.send("ready".encode('utf-8'))
+                if(game_stage == 1): #colocar as peças()
+                        time.sleep(1)
+                        if (player_turn == 1):
+                                print("p1")
+                                sock_c1.send("1".encode('utf-8'))
+                                sock_c2.send("1".encode('utf-8'))
+                        elif(player_turn == 2):
+                                print("p2")
+                                sock_c1.send("2".encode('utf-8'))
+                                sock_c2.send("2".encode('utf-8'))
+        player_turn = 1   
+        while (game_stage == 2):
+                if (player_turn == 1):
+                        time.sleep(1)
+                        sock_c1.send("1".encode('utf-8'))
+                        sock_c2.send("1".encode('utf-8'))
+                elif(player_turn == 2):
+                        time.sleep(1)
+                        sock_c1.send("2".encode('utf-8'))
+                        sock_c2.send("2".encode('utf-8')) 
+g = threading.Thread(target=game)
+g.start()
 server.serve_forever()
